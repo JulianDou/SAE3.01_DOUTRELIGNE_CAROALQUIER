9a99dddc15ddd6af34bb290e40f19aa070bc9642
EnguerranCA <enguerran.caroalquier@etu.unilim.fr>
2024-10-24 17:18:19 +0200

    Itération 7

--- api/Controller/CommandeController.php
+++ api/Controller/CommandeController.php
@@ -22,8 +22,11 @@ class CommandeController extends Controller
         $id = $request->getId("id");
 
         if ($id == "me") {
+            // On donne les commandes de l'utilisateur qui fait la requête
             return $this->products->findByUser();
-        }
+
+
+        } else
         if ($id) {
             $p = $this->products->find($id);
             return $p == null ? false : $p;
@@ -34,12 +37,34 @@ class CommandeController extends Controller
 
     protected function processPostRequest(HttpRequest $request)
     {
-    
+        $id = $request->getId("id");
 
+
+        // Si on a comme id status, on fait un update du statut de la commande
+        if ($id == "status") {
+
+            // On récupère les données JSON de la requête
+            $data = json_decode(file_get_contents('php://input'), true);
+            $idCommande = $data['id_commandes'];
+            $statut = $data['status'];
+
+
+            $commande = $this->products->find($idCommande);
+            
+            // Si la commande existe on la met à jour
+            if ($commande) {
+            $commande->setStatut($statut);
+            $ok = $this->products->update($commande);
+            return $ok ? $commande : false;
+            } else {
+            return false;
+            }
+        }
+
+    // Si on a pas d'id, on crée une nouvelle commande
 
     // Accéder aux données du tableau
     $products = json_decode($_POST['items'], true);
-
 
     // On vérifie que l'utilisateur est authentifié
     $clientId = $_SESSION['client']->getId();
